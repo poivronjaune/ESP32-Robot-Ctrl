d277696dd4da329f50cb3b0186e88ba2ed12fc90
PoivronJaune <poivronjaune@gmail.com>
2025-05-11 22:09:02 -0400

    Added yolo ultralytics object detection

--- brain.py
+++ brain.py
@@ -1,11 +1,14 @@
 import time
 import requests
+from ultralytics import YOLO
+
 BASE_URL = "http://192.168.1.31"
 
 def snap():
-    url_snap = BASE_URL + "/snap"
+    url_snap = BASE_URL + "/api/snap"
 
     res = requests.get(url_snap)
+
     if res.status_code == 200:
         with open("snap.jpg", "wb") as f:
             f.write(res.content)
@@ -43,11 +46,25 @@ def backward():
 
 
 if __name__ == "__main__":
-    stop()
-    input('Press Enter to move forward')
-    forward()
-    input('Press Enter to move backward')
-    backward()
-    input('Press Enter to stop')
-    stop()
-    
+    # stop()
+    # input('Press Enter to move forward')
+    # forward()
+    # input('Press Enter to move backward')
+    # backward()
+    # input('Press Enter to stop')
+    # stop()
+    snap()
+    input('Press Enter to run AI Detecttion')
+
+    model = YOLO('yolo12n.pt')  # Load model
+    results = model('.\snap.jpg') 
+
+    # Process results list
+    for result in results:
+        boxes = result.boxes                    # Boxes object for bounding box outputs
+        masks = result.masks                    # Masks object for segmentation masks outputs
+        keypoints = result.keypoints            # Keypoints object for pose outputs
+        probs = result.probs                    # Probs object for classification probabilities outputs
+        obb = result.obb                        # Oriented boxes object for oriented bounding boxes (OBB) outputs
+        # result.show()                         # Show annotated image
+        result.save(filename='snap-ai.jpg')     # Save image with bounding boxes
