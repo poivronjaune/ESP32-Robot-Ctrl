e610b64dabb1a0d3d92702a60f5b0d77ec6b00b7
poivronjaune <poivronjaune@gmail.com>
2025-05-25 13:51:36 -0400

    Create config.py

---
+++ ESP32-CAM/robot_server/config.py
@@ -0,0 +1,5 @@
+# Change these settings for you specific Wifi
+# BE careful when sharing your code this file will expose your credentials to the world
+
+SSID = 'Your local network'
+PASSWORD = 'Your network password'
